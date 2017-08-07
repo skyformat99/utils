8e18a3e2cf2685883cc9ecd5bdc9d6a806f25c1e
ccsexyz <mofaxyz@gmail.com>
2017-08-07 12:40:26 +0800

    add ExitCleaner

--- utils.go
+++ utils.go
@@ -3,6 +3,7 @@ package utils
 import (
 	"crypto/rand"
 	"encoding/binary"
+	"sync"
 )
 
 const defaultMethod = "aes-256-cfb"
@@ -18,4 +19,48 @@ func GetRandomBytes(len int) []byte {
 	data := make([]byte, len)
 	PutRandomBytes(data)
 	return data
+}
+
+type ExitCleaner struct {
+	lock   sync.Mutex
+	runner []func()
+}
+
+func (c *ExitCleaner) Push(f func()) int {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	n := len(c.runner)
+	c.runner = append(c.runner, f)
+	return n
+}
+
+func (c *ExitCleaner) Exit() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	for i := len(c.runner) - 1; i >= 0; i-- {
+		f := c.runner[i]
+		if f == nil {
+			continue
+		}
+		f()
+	}
+}
+
+func (c *ExitCleaner) Delete(index int) func() {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	if index > len(c.runner) || index < 0 {
+		return nil
+	}
+	f := c.runner[index]
+	if index == 0 {
+		c.runner = c.runner[1:]
+	} else if index == len(c.runner)-1 {
+		c.runner = c.runner[:index]
+	} else {
+		runner1 := c.runner[:index]
+		runner2 := c.runner[index+1:]
+		c.runner = append(runner1, runner2...)
+	}
+	return f
 }
